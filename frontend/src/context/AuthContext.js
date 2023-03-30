2c186135aba8b86a77d1d0f5b4118137ff8abfa4
Nguyen Anh Thu <nightcore1506@gmail.com>
2023-03-30 07:23:45 +0700

    new

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,12 +1,12 @@
 import {createContext , useEffect ,useReducer} from 'react'
 
 const initial_state = {
-    user:null ,
+    user:localStorage.getItem('user') !== undefined ? JSON.parse(localStorage.getItem('user')):null,
     loading:false,
     error:null,
 }
 export const AuthContext = createContext (initial_state)
-const AuthContext = (state, action)=>{
+const AuthReducer = (state, action)=>{
     switch (action.type){
         case "LOGIN_START":
             return {
@@ -47,5 +47,14 @@ export const AuthContextProvider = ({children})=>{
     const [state, dispatch] = useReducer (AuthReducer, initial_state)
     useEffect (()=>{
         localStorage.setItem('user',JSON.stringify(state.user))
-    }),[state.user]
+    },[state.user])
+    return(<AuthContext.Provider value={{
+        user:state.user,
+        loading: state.error.loading,
+        error:state.error,
+        dispatch
+    }}>
+        {children}
+    </AuthContext.Provider>
+    )
 }
