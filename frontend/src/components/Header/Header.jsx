65c7a70aaca7db4d2373f694cf9122b839003034
Nguyen Anh Thu <nightcore1506@gmail.com>
2023-04-02 18:11:56 +0700

    Logout

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import React, {useRef,useEffect} from 'react';
+import React, {useRef,useEffect, useContext} from 'react';
 import {Container,Row,Button} from 'reactstrap'
-import {NavLink, Link} from 'react-router-dom'
+import {NavLink, Link,useNavigate} from 'react-router-dom'
 import logo from '../../assets/images/logo4.png'
 import './header.css'
+import { AuthContext } from '../../context/AuthContext';
 const Nav__link=[
   {
     path:'/home',
@@ -16,12 +17,20 @@ const Nav__link=[
     path:'/tours',
     display:'Tours'
   },
-
 ];
 
 
 const Header = () => {
       const headerRef = useRef(null)
+      const navigate = useNavigate()
+      const {user, dispatch} = useContext(AuthContext)
+
+      const logout = () =>{
+        dispatch({type:'LOGOUT'})
+        navigate('/')
+      }
+
+
       const stickyHeaderFunc =()=>{
         window.addEventListener('scroll',()=>{
           if(document.body.scrollTop >80|| document.documentElement.scrollTop >80){
@@ -61,8 +70,14 @@ const Header = () => {
           {/*================= menu end =================*/}
            <div className="nav__right d-flex align-items-center gap-3">
             <div className="nav__btns d-flex align-items-center gap-3">
-              <Button className="btn secondary__btn"><Link to ='/login'>login</Link></Button>
-              <Button className="btn primary__btn"><Link to ='/register'>Register</Link></Button>
+                {
+                  user?<>
+                  <h5 className="mb-0">{user.username}</h5>
+                  <Button className='btn btn-dark' onClick={logout}>Logout</Button>
+                  </>: (<>
+                  <Button className="btn secondary__btn"><Link to ='/login'>login</Link></Button>
+                  <Button className="btn primary__btn"><Link to ='/register'>Register</Link></Button>
+                  </>)}
             </div>
             <span className="mobile__menu"></span>
             <i class="ri-menu-add-fill"></i>
