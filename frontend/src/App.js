dd09ac1e47ed39cbed07807acd9490fca9ecf2cf
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2023-04-07 23:48:48 +0700

    install redux toolkit, antdesign, node-sass, sass-loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import HomeTemplate from "./template/index";
+import HomeTemplate from "./template/HomeTemplate/index";
+import AdminTemplate from "./template/AdminTemplate/index";
 import Home from "./pages/Home/index";
 import Login from "./pages/Login/index";
 import Register from "./pages/Register/index";
 import About from "./pages/About/index";
 import Tours from "./pages/Tours/index";
-import ToursDetails from "./pages/TourDetails/ToursDetails";
+import ToursDetails from "./pages/TourDetails/index";
 import ThankYou from "./pages/ThankYou/index";
 import SearchResultList from "./pages/Search/index";
 
@@ -16,14 +17,15 @@ function App() {
     <Routes>
       <Route path="" element={<HomeTemplate />}>
         <Route index element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/tours" element={<Tours />} />
-        <Route path="/tours/:id" element={<ToursDetails />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/tours/search" element={<SearchResultList />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="about" element={<About />} />
+        <Route path="tours" element={<Tours />} />
+        <Route path="tours/:id" element={<ToursDetails />} />
+        <Route path="thank-you" element={<ThankYou />} />
+        <Route path="tours/search" element={<SearchResultList />} />
       </Route>
+      <Route path="admin" element={<AdminTemplate />}></Route>
     </Routes>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "antd/dist/reset.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import { AuthContextProvider } from "./context/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </AuthContextProvider>
   </React.StrictMode>
